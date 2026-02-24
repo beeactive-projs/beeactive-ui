c84aafb0b4bc642b8aa8fa4d9327661c51db60e3
Bogdan Daniel <daniel_bogdan7@yahoo.com>
2026-02-24 16:22:18 +0200

    scroll fix

--- projects/web/src/app/app.config.ts
+++ projects/web/src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { providePrimeNG } from 'primeng/config';
 
@@ -11,7 +11,7 @@ import { BeeActiveLara } from '../styles.primeng';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'top' })),
     provideHttpClient(withInterceptors([authInterceptor])),
     providePrimeNG({
       theme: {
